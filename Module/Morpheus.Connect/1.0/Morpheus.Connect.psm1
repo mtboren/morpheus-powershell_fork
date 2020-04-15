f35db9d8f577762544681cf50890ce2a449d3b4a
MattBoren <technology@ddayinc.com>
2020-04-15 14:50:37 -0400

    add support for API Token authentication to Connect-Morpheus

--- Module/Morpheus.Connect/1.0/Morpheus.Connect.psm1
+++ Module/Morpheus.Connect/1.0/Morpheus.Connect.psm1
@@ -1,7 +1,4 @@
-﻿New-Variable -Name URL
-New-Variable -Name Header
-
-Function Connect-Morpheus {
+﻿Function Connect-Morpheus {
     <#
     .Synopsis
        Makes connection to your Morpheus Appliance.
@@ -11,56 +8,79 @@ Function Connect-Morpheus {
     .EXAMPLE
        Connect-Morpheus -URL test.morpheus.com
     .EXAMPLE
+       Connect-Morpheus -URL test.morpheus.com -ApiCredential (Get-Credential)
+       Connect to the given Morpheus instance using an API access token. Note:  the _password_ portion of the ApiCredential parameter value is used as the API access token
+    .EXAMPLE
        Connect-Morpheus -URL https://test.morpheus.com -Username TestUser
     .EXAMPLE
        Connect-Morpheus -URL https://test.morpheus.com -Username TestUser -Password S@mplePa55
     #>
 
 
-    ####  User Variables  ####
-    Param(
-        [Parameter(Mandatory=$true)][string]$URL,        
-        [Parameter(Mandatory=$true)][string]$Username,
-        $Password
-        )
-    if (!$URL.StartsWith('https://')) {
-        $URL = ('https://' + $URL)
-        }
+    Param (
+        ## Morpheus data destination to which to connect
+        [Parameter(Mandatory=$true)][string]$URL,
 
-    $Script:URL = $URL
+        ## Username for connecting with username/password pair
+        [Parameter(Mandatory=$true, ParameterSetName="AuthWithUser")][string]$Username,
 
-    if (-not($Password)) {
-        $Password = Read-host 'Enter Password' -AsSecureString
-        $PlainTextPassword= [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR( ($Password) ))
-        }
-    ELSE {
-        $PlainTextPassword = $Password
-        }
+        ## Password to use for username/password authentication
+        [parameter(ParameterSetName="AuthWithUser")]$Password,
 
-    Try {
-        $Errors = $null
-        ####  Morpheus Variables  ####
-        $Body = "username=$Username&password=$PlainTextPassword"
-        $AuthURL = "/oauth/token?grant_type=password&scope=write&client_id=morph-customer"
+        ## Credential with API access token as the password, for connecting to Morpheus with an API access token
+        [parameter(Mandatory=$true, ParameterSetName="AuthWithApiToken")][System.Management.Automation.PSCredential]$ApiCredential
+    )
 
-        ####  Create User Token   ####
-        $Token = Invoke-WebRequest -Method POST -Uri ($URL + $AuthURL) -Body $Body | select -ExpandProperty content|
-            ConvertFrom-Json | select -ExpandProperty access_token
-        $Script:Header = @{
-            "Authorization" = "BEARER $Token"
+    process {
+        if (!$URL.StartsWith('https://')) {
+            $URL = ('https://' + $URL)
             }
-        }
 
-    Catch {
-        $Errors = $true
-        Write-Host "Failed to authenticate credentials" -ForegroundColor Red
-        }
-    Finally {
-        if (!$Errors) {
-            Write-Host "Successfully connected to $URL.
-Use `"Get-Command -Module Morpheus`" to discover available commands." -ForegroundColor Yellow
-            }
-        }
-    }
+        $global:URL = $URL
 
-Export-ModuleMember -Variable URL,Header
+        ## if using API access token
+        if ($PSCmdlet.ParameterSetName -eq "AuthWithApiToken") {
+            $global:Header = @{
+                ## use the ApiCredential "password" field, which is the API access token
+                "Authorization" = ("BEARER {0}" -f $ApiCredential.GetNetworkCredential().Password)
+                }
+        } ## end if
+        ## else, using username/password
+        else {
+            if (-not($Password)) {
+                $Password = Read-host 'Enter Password' -AsSecureString
+                $PlainTextPassword= [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR( ($Password) ))
+                }
+            else {
+                $PlainTextPassword = $Password
+                }
+
+            Try {
+                $Errors = $null
+                ####  Morpheus Variables  ####
+                $Body = "username=$Username&password=$PlainTextPassword"
+                $AuthURL = "/oauth/token?grant_type=password&scope=write&client_id=morph-customer"
+
+                ####  Create User Token   ####
+                $Token = Invoke-WebRequest -Method POST -Uri ($URL + $AuthURL) -Body $Body | select -ExpandProperty content|
+                    ConvertFrom-Json | select -ExpandProperty access_token
+                $global:Header = @{
+                    "Authorization" = "BEARER $Token"
+                    }
+                }
+
+            Catch {
+                $Errors = $true
+                Write-Host "Failed to authenticate credentials" -ForegroundColor Red
+                }
+            Finally {
+                if (!$Errors) {
+                    Write-Host "Successfully connected to $URL.`nUse `"Get-Command -Module Morpheus`" to discover available commands." -ForegroundColor Yellow
+                    }
+                }
+        } ## end else
+    } ## end process
+}
+
+## export these via the PowerShell Data file (psd1)
+# Export-ModuleMember -Variable URL,Header
